ac70a762bb49a1c180c52c39694ab17bfb8a0ea8
vijayarya01@live.com <vijayarya01@live.com>
2022-09-03 09:24:01 +0530

    coming from MAC - unittest

--- UnitTestPy/TestCase2.py
+++ UnitTestPy/TestCase2.py
@@ -1,5 +1,26 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 
 class SearchEnginesTest(unittest.TestCase):
     def test_Google(self):
+        # initialize chrome driver manager
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get("https://www.google.com/")
+        print("the title of page is ", self.driver.title)
+        time.sleep(5)
+        self.driver.close()
+
+    def test_Bing(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get("https://www.bing.com")
+        print("The title of page is " , self.driver.title)
+        time.sleep(5)
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
