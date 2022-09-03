4ee6010b79d688d197ff4ab8c2cba8feb8d79e7b
vijayarya01@live.com <vijayarya01@live.com>
2022-09-03 09:07:05 +0530

    coming from MAC - unittest

---
+++ UnitTestPy/TestCase1.py
@@ -0,0 +1,8 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test_FirstTest(self):
+        print("This is my first unit test case")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ UnitTestPy/TestCase2.py
@@ -0,0 +1,5 @@
+import unittest
+from selenium import webdriver
+
+class SearchEnginesTest(unittest.TestCase):
+    def test_Google(self):
